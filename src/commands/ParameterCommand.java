b168e02a66c0765a0971f22fee14d930fa677d28
Matthew Allen <AechPro@users.noreply.github.com>
2016-09-14 13:06:31 -0400

    Create ParameterCommand.java
    Initial commit.

---
+++ src/commands/ParameterCommand.java
@@ -0,0 +1,13 @@
+package commands;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class ParameterCommand extends Command
+{
+	public ParameterCommand(String m, File f, String n, int maxU, ArrayList<String> params)
+	{
+		super(m, f, n, maxU);
+	}
+	
+}
