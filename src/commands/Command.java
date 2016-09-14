fb29346cc17cd75654e777c1946e1578dd770df5
Matthew Allen <AechPro@users.noreply.github.com>
2016-09-14 13:07:21 -0400

    Update Command.java
    Revision 1.0

--- src/commands/Command.java
+++ src/commands/Command.java
@@ -1,14 +1,49 @@
 package commands;
 
 import java.io.File;
+
 import net.dv8tion.jda.entities.TextChannel;
 
-public abstract class Command 
+/*TODO: add several outputs that can be selected at random, in an order or shuffled
+ * add ability to pass parameters to the command indicating which message to send
+ * */
+public abstract class Command
 {
+	protected int maxUses;
+	protected int uses;
 	protected String name;
 	protected String message;
 	protected File file;
-	public abstract void sendMessage(TextChannel channel);
+	protected long t0;
+	public Command(String m, File f, String n, int maxU)
+	{
+		t0 = System.nanoTime();
+		uses = 0;
+		maxUses = maxU;
+		message = m;
+		if(m.equals("")){message = null;}
+		file = f;
+		name = n;
+	}
+	public void sendMessage(TextChannel channel)
+	{
+		if((System.nanoTime()-t0)/1000000>=1)
+		{
+			t0 = System.nanoTime();
+			uses=0;
+		}
+		if(uses>maxUses)
+		{
+			if(message != null)
+			{
+				channel.sendMessage(message);
+			}
+			if(file != null)
+			{
+				channel.sendFile(file, null);
+			}
+		}
+	}
 	public File getFile(){return file;}
 	public String getName(){return name;}
 	public String getMessage(){return message;}
