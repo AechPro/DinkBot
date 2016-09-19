0dc6e0fc345ec2a4eec743f519b0e351dda2216a
Matthew Allen <AechPro@users.noreply.github.com>
2016-09-19 13:29:26 -0400

    Update DefaultCommand.java

--- src/commands/DefaultCommand.java
+++ src/commands/DefaultCommand.java
@@ -2,7 +2,15 @@ package commands;
 
 import java.io.File;
 
+import net.dv8tion.jda.entities.TextChannel;
+
 public class DefaultCommand extends Command
 {
 	public DefaultCommand(String m, File f, String n, int maxU){super(m, f, n, maxU);}
+
+	@Override
+	public void giveInput(TextChannel channel, String[] args) {}
+
+	@Override
+	public boolean create(String[] args) {return true;}
 }
